40c3f584069afc55dc04960cc17c20f8730e79b4
Nicolas Frache <frache.nicolas@gmail.com>
2020-09-28 22:02:00 +0200

    Correction chemin absolu en relatif :D

--- Solo_Noble/src/SoloNoble.java
+++ Solo_Noble/src/SoloNoble.java
@@ -4,7 +4,7 @@ public class SoloNoble {
 	
 	public static void main(String[] args) {
 		try {
-			Grille g = new Grille("/Users/silviobrancati/Documents/IUT/2020_2021/Algo/TP_Solo_Noble/SoloNoble/tablier.txt");
+			Grille g = new Grille("./tablier.txt");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
